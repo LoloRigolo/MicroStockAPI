212572b27d743db307f0fac81a1c33b0bdfaa9a0
nolan25 <favandnolan@gmail.com>
2024-12-03 10:14:50 +0100

    fix bug

--- marchandises/models/marchandise.js
+++ marchandises/models/marchandise.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const marchandiseSchema = new mongoose.Schema({
     nom: { type: String, required: true, unique: true },
-    prix: { type: Number, required: true },
-    volume: { type: Number, required: true }
+    prix: { type: Number, required: true }
 });
 
 const marchandise = mongoose.model('marchandise', marchandiseSchema);
